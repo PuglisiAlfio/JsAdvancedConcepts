840b46f666274e3ad99a2385c19de8dc1169f8ca
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-07 12:47:48 +0100

    Esercizio 1

---
+++ es-01/exercise-1.js
@@ -0,0 +1,36 @@
+// ceo => 2200
+// manager => 1800
+// cto => 1800
+// developer => 1500
+// default => 1000
+function calculateSalary(role) {
+  // ...
+  switch (role){
+    case "ceo":
+      return `Il ${role} guadagna 2200€`;
+      break;
+    case 'manager':
+      return `Il ${role} guadagna 1800€`;
+      break;
+    case 'cto':
+      return `Il ${role} guadagna 1800€`;
+      break;
+    case 'developer':
+      return `Il ${role} guadagna 1500€`;
+      break;
+    default:
+      return `The ${role} earn 1000€`;
+  }
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
