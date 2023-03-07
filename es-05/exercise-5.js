eb0453a145b1040a6e5b36d157bce0233a5446ce
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-07 15:23:23 +0100

    spiegazione perché person2=person1

--- es-05/exercise-5.js
+++ es-05/exercise-5.js
@@ -5,7 +5,7 @@ const person1 = {
 };
 
 // const person2 = person1;
-// se facessimo come sopra diremo che person2 è uguale a person1 quindi se cambiassimo il nome in person2 (essendo uguale a person1) cambierebbe automaticamente anche il name di person1
+// se facessimo come sopra diremo che person2 è uguale a person1 quindi se cambiassimo il nome di person2 (essendo uguale a person1) cambierebbe automaticamente anche il name di person1
 const person2 = Object.assign({}, person1);
 
 person2.firstName = "Simon";
