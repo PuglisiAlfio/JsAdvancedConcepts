05ac73b6e2b0febb2fccf4e881def1be9102473e
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-07 12:58:36 +0100

    Esercizio 05

---
+++ es-05/exercise-5.js
@@ -0,0 +1,15 @@
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+// const person2 = person1;
+// se facessimo come sopra diremo che person2 è uguale a person1 quindi se cambiassimo il nome in person2 (essendo uguale a person1) cambierebbe automaticamente anche il name di person1
+const person2 = Object.assign({}, person1);
+
+person2.firstName = "Simon";
+// Modify the property `firstName` of the `person2` in "Simon".
+
+console.log(person1);
+console.log(person2);
