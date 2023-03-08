62d5f8748eb4171c928cc663ba2ce54e874d257c
Alfio Puglisi <puglisi_algio@ciao.it>
2023-03-08 17:14:38 +0100

    Esercizio 10 con optional chaining

--- es-10/exercise-10.js
+++ es-10/exercise-10.js
@@ -1,17 +1,12 @@
 const order = {
-  customer: {
-    address: {
-      city: {}
-    }
-  }
+  customer: {},
+  address: {},
+  city: {}
 };
 
-// if (order && order.customer && order.customer.address && !order.customer.address.city) {
-//   console.log('City is required');
-// } else if (order && order.customer && order.customer.address && order.customer.address.city){
-//   console.log('City is already declared');
-// }
+if (order?.customer?.address?.city) {
+  console.log('City is required');
+ } else {
+  console.log('niente da fare');
+ }
 
-let condition = order && order.customer && order.customer.address && !order.customer.address.city ? 'City is required' : 'City is already declared';
-
-console.log(condition);
